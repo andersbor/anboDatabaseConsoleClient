3edde11c75ffc994bf83cc22b4eb77eaaa14641c
andersbor <anbo@easj.dk>
2017-09-19 19:57:25 +0200

    Add project files.

---
+++ Anbo1DatabaseConsoleClient/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Anbo1DatabaseConsoleClient
+{
+    class Program
+    {
+        private const string ConnectionString =
+            "Server=tcp:anbo-databaseserver.database.windows.net,1433;Initial Catalog=anbobase;Persist Security Info=False;User ID=anbo;Password=Secret12;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private const string SelectAllStudents = "select * from student";
+        private const string InsertStudent = "insert into student (name, semester) values (@name, @semester)";
+
+        static void Main()
+        {
+            //string connString = GetConnectionStringFromAppConfig();
+            string connString = ConnectionString;
+            using (SqlConnection databaseConnection = new SqlConnection(connString))
+            {
+                databaseConnection.Open();
+                ShowAllStudents(databaseConnection);
+                InsertStudents(databaseConnection);
+                ShowAllStudents(databaseConnection);
+            }
+        }
+
+        private static string GetConnectionStringFromAppConfig()
+        {
+            // https://msdn.microsoft.com/en-us/library/ms254494.aspx
+            ConnectionStringSettingsCollection connectionStringSettingsCollection = ConfigurationManager.ConnectionStrings;
+            ConnectionStringSettings connStringSettings = connectionStringSettingsCollection["anbo1databaseAzure"];
+            string connString = connStringSettings.ConnectionString;
+            return connString;
+        }
+
+        private static IList<Student> ShowAllStudents(SqlConnection databaseConnection)
+        {
+            using (SqlCommand selectCommand = new SqlCommand(SelectAllStudents, databaseConnection))
+            {
+                using (SqlDataReader reader = selectCommand.ExecuteReader())
+                {
+                    IList<Student> students = new List<Student>();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32(0);
+                            string name = reader.GetString(1);
+                            int semester = reader.GetInt32(2);
+                            DateTime timeStamp = reader.GetDateTime(3);
+                            // normally you would not show the ID to the user
+                            Console.WriteLine(id + " " + name + " " + semester + " " + timeStamp);
+                            Student st = new Student()
+                            {
+                                
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No rows");
+                    }
+                }
+            }
+        }
+        private static void InsertStudents(SqlConnection databaseConnection)
+        {
+            while (true)
+            {
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(name)) break;
+                Console.Write("Semester: ");
+                string semester = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(semester)) break;
+                using (SqlCommand insertCommand = new SqlCommand(InsertStudent, databaseConnection))
+                {
+                    insertCommand.Parameters.AddWithValue("@name", name);
+                    insertCommand.Parameters.AddWithValue("@semester", semester);
+                    int rowsAffected = insertCommand.ExecuteNonQuery();
+                    Console.WriteLine(rowsAffected + " row(s) affected");
+                }
+            }
+        }
+    }
+}
